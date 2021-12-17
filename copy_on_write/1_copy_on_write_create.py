cd9be82f1295d545dcd2a05d80f9655a03e58c94
Christopher Sharkey <ev2900@gmail.com>
2021-12-16 22:23:10 -0500

    simplification to the write data script

--- copy_on_write/1_copy_on_write_create.py
+++ copy_on_write/1_copy_on_write_create.py
@@ -18,7 +18,7 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 #
-# Read the example data
+# Create sample data that we can use to write to S3 as a Hudi dataset
 #
 
 # Option #1 ----- Create a Sample Data in this Script ----
@@ -41,44 +41,10 @@ schema = StructType([
 
 inputDf = spark.createDataFrame(data=data,schema=schema)
 
-# Option #2 ----- Read from Glue Catalog ----
+#
+# Write data to S3 as a Hudi dataset
+# 
 
-'''
-input_Dynamic_Frame = glueContext.create_dynamic_frame.from_catalog(database = "hudi-glue", table_name = "yellow_cab_input_data", transformation_ctx = "transformation_0")
-inputDf = input_Dynamic_Frame.toDF()
-inputDf = inputDf.withColumn("pk_col",monotonically_increasing_id() + 1)
-'''
-
-# Option #3 ----- Read directly from S3 ----
-'''
-# Schema for NYC Taxi Data
-yt_schema = StructType([
-    StructField("vendorid",IntegerType(),True),
-    StructField("tpep_pickup_datetime",TimestampType(),True),
-    StructField("tpep_dropoff_datetime",TimestampType(),True),
-    StructField("passenger_count", IntegerType(), True),
-    StructField("trip_distance", DoubleType(), True),
-    StructField("ratecodeid", IntegerType(), True),
-    StructField("store_and_fwd_flag", StringType(), True),
-    StructField("pulocationid", IntegerType(), True),
-    StructField("dolocationid", IntegerType(), True),
-    StructField("payment_type", IntegerType(), True),
-    StructField("fare_amount", DoubleType(), True),
-    StructField("extra", DoubleType(), True),
-    StructField("mta_tax", DoubleType(), True),
-    StructField("tip_amount", DoubleType(), True),
-    StructField("tolls_amount", DoubleType(), True),
-    StructField("improvement_surcharge", DoubleType(), True),
-    StructField("total_amount", DoubleType(), True),
-    StructField("congestion_surcharge", DoubleType(), True),
-    StructField("pk_col", LongType(), True)
-])
-
-# Read the data
-inputDf = spark.read.schema(yt_schema).option("header", "true").csv("s3://hudi-chrisshark-glue/yellow_cab_input_data/yellow_tripdata_2020-01.csv").withColumn("pk_col",monotonically_increasing_id() + 1)
-'''
-
-# ----- If you are using Option 1 for sample data use the configurations below
 # Set hudi options
 hudiOptions = {
     'className' : 'org.apache.hudi',
@@ -92,23 +58,6 @@ hudiOptions = {
     'hoodie.datasource.hive_sync.enable': 'true',
     'path': 's3://hudi-chrisshark-glue/copy_on_write_glue'
 }
-
-# ----- If you are using Option 2 or 3 for sample data use the configurations below
-'''
-# Set hudi options
-hudiOptions = {
-    'className' : 'org.apache.hudi',
-    'hoodie.datasource.hive_sync.use_jdbc':'false',
-    'hoodie.datasource.write.precombine.field': 'tpep_pickup_datetime',
-    'hoodie.datasource.write.recordkey.field': 'pk_col',
-    'hoodie.table.name': 'copy_on_write_glue',
-    'hoodie.consistency.check.enabled': 'true',
-    'hoodie.datasource.hive_sync.database': 'default',
-    'hoodie.datasource.hive_sync.table': 'copy_on_write_glue',
-    'hoodie.datasource.hive_sync.enable': 'true',
-    'path': 's3://hudi-chrisshark-glue/copy_on_write_glue'
-}
-'''
 
 unpartitionDataConfig = {
     'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.NonPartitionedExtractor',
